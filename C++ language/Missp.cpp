9faaa59d4586cea13345bf1671579f9a5b102717
Anirban Basak <jeetbasak2002@gmail.com>
2022-07-27 17:57:06 +0530

    27/07/2022

--- C++ language/Missp.cpp
+++ C++ language/Missp.cpp
@@ -1,25 +1,19 @@
 #include <iostream>
 using namespace std;
-
 int main(){
     int T;
     long long N;
     cout << ":";
     cin >> T;
-
     while(T--){
         cin >> N;
+        int count = 0;
         int dolls_left[N];
-        for(int i = 0;i<N;i++)
-            cin >> dolls_left[i];
-
-        int count[N];
-        int flag[N];
-
         for(int i = 0;i<N;i++){
-            
+            cin >> dolls_left[i];
+            count = count ^ dolls_left[i];
         }
+        cout << count << "\n";
     }
-
     return 0 ;
 }
