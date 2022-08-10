a91eff2600daa7fab9ec168d31a1a1f50dca8277
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-10 19:26:49 +0530

    10/08/2022

---
+++ C++ language/Icm0013.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+int main(){
+    int T;
+    cout << ":";
+    cin >> T;
+    while(T--){
+        int N;
+        cin >> N;
+        cout << N << endl;
+    }
+    return 0 ;
+}
