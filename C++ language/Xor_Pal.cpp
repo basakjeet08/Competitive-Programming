25ee7ff9332c83002aaac6ccf075429688fdef19
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-13 06:48:03 +0530

    13/08/2022

---
+++ C++ language/Xor_Pal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std ;
+int main(){
+    int T ;
+    cout << T ;
+    while(T--){
+        int N;
+        cin >> N ;
+        string S ;
+        cin >> S ;
+        
+    }
+
+
+
+
+    return 0 ;
+}
