6b327d7a09d6d0530c50de5c805483eab3f56ba4
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-14 15:57:03 +0530

    14/08/2022

--- C++ language/Smallxor.cpp
+++ C++ language/Smallxor.cpp
@@ -6,10 +6,18 @@ int main(){
     cin >> T ;
     while(T--){
         int N,X,Y;
-        cin >> N >> X >> Y;
-        int A[N];
-        for(int i =0;i<N;i++)
-            cin >> A[i];
+        // cin >> N >> X >> Y;
+        
+        // int A[N];
+        // for(int i =0;i<N;i++)
+        //     cin >> A[i];
+
+
+
+        cin >> X ;
+        for(int i=1;i<100;i++){
+            cout << (i^X) << " ";
+        }
     }
 
 
