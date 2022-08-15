d89c03a148f5196cb099e8687b6e4d08a1a7015d
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-15 23:50:43 +0530

    15/08/2022

--- C++ language/Datatype.cpp
+++ C++ language/Datatype.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-using namespace std ;
-int main(){
-    int T ;
-    cout << ":" ;
-    cin >> T ;
-    while(T--){
-        int N,X;
-        cin >> N >> X ;
-        if(N >= X)
-            cout << X << endl ;
-        else
-            cout << (X-N-1) << endl ;
-    }
-    return 0 ;
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  int T;
+  cout << ":";
+  cin >> T;
+  while(T--){
+      int N,X;
+      cin >> N >> X;
+      int ans=X%(N+1);
+      cout<<ans<<endl;
+  }
+	return 0;
 }
