ed0cc924a89c731ef7abc5e5fe8ba7a2558449fa
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-17 19:56:55 +0530

    17/08/2022

---
+++ C++ language/Cielab.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std ;
+int main(){
+    int T ;
+    cout << ":";
+    cin >> T ;
+    while(T--){
+        int A,B;
+        cin >> A >> B ;
+        int value = abs(A-B);
+        if(value%10 == 9)
+            value--;
+        else if(value%10 == 0)
+            value++;
+        else
+            value++;
+        cout << value << endl;
+    }
+
+
+
+    return 0 ;
+}
