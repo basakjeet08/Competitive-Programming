954a577e919f5e4a97008b259849801d1aae9f95
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-17 19:49:18 +0530

    17/08/2022

--- C++ language/Gameofpiles.cpp
+++ C++ language/Gameofpiles.cpp
@@ -9,17 +9,22 @@ int main(){
         cin >> N ;
         long long A[N];
         long long total = 0 ;
-        for(int i=0;i<N;i++){
+        for(int i=0;i<N;i++)
             cin >> A[i];
+        int flag = 0 ;
+        for(int i=0;i<N;i++){
             total += A[i];
+            if(A[i] == 1){
+                flag = 1 ;
+                break;
+            }
         }
-        if(total%2 == 0)
-            cout << "CHEF \n" ;
+        if(flag)
+            cout << "CHEF \n";
+        else if(total%2 == 0)
+            cout << "CHEFINA \n" ;
         else
-            cout << "CHEFINA \n";
-        
+            cout << "CHEF \n";   
     }
-
-
     return 0 ;
 }
