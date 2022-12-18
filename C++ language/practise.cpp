ae07a1c50547dbb066407f957b067b8087b73d29
Anirban Basak <jeetbasak2002@gmail.com>
2022-12-19 02:17:29 +0530

    joint commit

---
+++ C++ language/practise.cpp
@@ -0,0 +1,16 @@
+#include <stack>
+#include <queue>
+#include <deque>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    deque<int> s;
+    s.push_back(0);
+    s.push_back(10);
+    s.push_back(20);
+    s.push_back(30);
+    s.push_back(40);
+    s.push_back(50);
+    for(int i :  s)
+        cout << i << endl ;
+}
