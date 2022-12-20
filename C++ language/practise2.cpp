097130418af6ac52b93ebcc36e57e2196e005ffc
Anirban Basak <jeetbasak2002@gmail.com>
2022-12-20 11:03:11 +0530

    20/12/2022

--- C++ language/practise2.cpp
+++ C++ language/practise2.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
-using namespace std;
-struct Node{
-    int data ;
-    struct Node* next ;
+using namespace std ;
 
+struct ListNode {
+    int val;
+    ListNode *next;
 };
-int main(){
-    Node* head = new Node;
-    head->data = 10;
-    head->next = NULL ;
-    Node* tail = new Node ;
-    tail->next = NULL ;
-
-    cout << tail->data ;
-}
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        struct ListNode* ptr1 = l1 ;
+        struct ListNode* ptr2 = l2 ;
+        struct ListNode* head3 = new ListNode ;
+        struct ListNode* ptr3 = head3 ;
+        int carry = 0 ;
+        while(ptr1 != NULL && ptr2 != NULL){
+            int add = ptr1->val + ptr2->val + carry;
+            struct ListNode* newNode = new ListNode ;
+            newNode->next = NULL ;
+            if(add >= 10){
+                newNode->val = add-10;
+                carry = 1 ;
+            }
+            if(head3 == NULL)
+                head3 = ptr3 = newNode;
+            else{
+                ptr3->next = newNode;
+                ptr3 = newNode;
+            }
+        }
+        return head3 ;
+    }
+};
